18da84d06a742b5c68d367e3093cb6754c260c0a
Shau Yin Luk <ivan.luk@icloud.com>
2020-11-27 10:19:35 -0600

    user full crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,40 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    # this method is called when user refresh on browser
+    # use token send to find user and send back same JSON data as successful login
+    token = request.headers['Authorization'].split(' ').last
+    decoded_token = JWT.decode(token, 'g0lf_3uck$t', true, { algorithm: 'HS256' })
+    id = decoded_token.first['user_id']
+
+    user = User.find(id)
+    if user
+      render json: { id: user.id, username: user.username, zipcode: user.zipcode, jwt: token }
+    else
+      render json: { error: 'Invalid token'}
+    end
+  end
+
+  def signup
+    #  this method is called when user signup
+    user_new = User.create!(username: params[:username], password: params[:password], zipcode: params[:zipcode])
+    if user_new 
+      # after user is created, create token and send back JSON data as successful login
+      token = encode_token({ user_id: user_new.id })
+      render json: { id: user_new.id, username: user_new.username, zipcode: user_new.zipcode, jwt: token }
+    end
+  end
+
+  def update
+    #  this method is called when a user is updated
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user
+    # this doesn't work
+    # render jason: { id: user.id, username: user.username, email: user.email, handicap: user.my_handicap, clubs: user.clubs}
+  end
+  
   def user_params
     params.require(:user).permit(:username, :password, :zipcode)
   end
