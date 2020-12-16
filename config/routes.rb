a65b7d0ffc92892d5ca273ab4e423e3a09b3c5ff
Shau Yin Luk <ivan.luk@icloud.com>
2020-12-16 12:00:20 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :reports
+
+  get '/current_user', to: 'users#show'
+
+  post '/users', to: 'users#create'
+
+  post '/signup', to: 'users#signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
