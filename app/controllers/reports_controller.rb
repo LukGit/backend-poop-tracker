56f334634bb5ecadc8b150a2860c3c3a332bd1c7
Shau Yin Luk <ivan.luk@icloud.com>
2021-01-08 10:43:09 -0600

    report create in controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,4 +5,15 @@ class ReportsController < ApplicationController
     render json: reports
   end
 
+  def create
+    # this method is called when a new bucket item is added
+    report = Report.create!(report_params)
+    render json: report
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:poop_lat, :poop_lng, :user_id, :poop_size)
+  end
 end
